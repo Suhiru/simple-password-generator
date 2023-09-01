fd6b3e10e3d0aa8936f64e1aef302b5b0169717c
Suhiru Benjamin De Silva <suhirudesilva@gmail.com>
2023-09-02 00:23:35 +0200

    event listeners added to the buttons
    event listeners added to the buttons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const btn_generation = document.querySelector('#generation');
+const btn_copy = document.querySelector('#copy');
+
+window.onload = init;
+
+function init() {
+  btn_generation.addEventListener('click', main);
+  btn_copy.addEventListener('click', copy);
+}
+
+const main = () => {
+  console.log('Generated');
+};
+
+const copy = () => {
+  console.log('copied');
+};
